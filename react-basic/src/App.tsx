aa84e13900ee8d09040d8598335b7398dd2f6c20
jiraynor <jiraynor@naver.com>
2023-07-10 15:21:09 +0900

    2023.07.10. 15:20

--- react-basic/src/App.tsx
+++ react-basic/src/App.tsx
@@ -1,5 +1,5 @@
 import './App.css';
-import TestComponent from './components/TestComponent';
+import TestComponent, { testVariable } from './components/TestComponent';
 import './style.css';
 import Footer from './views/layout/Footer';
 import Header from './views/layout/Header';
@@ -9,11 +9,13 @@ function App() {
 
   return (
     <>
-      <TestComponent arg1='문자열' arg2={1} />
-      <TestComponent arg1='문자열2' arg2={100} />
-      <Header />
+      {
+        // <TestComponent arg1={'문자열'} />
+        // <TestComponent arg1={'문자열2'} arg2={100} />
+      }
+      <Header count={0} />
       <Main />
-      <Footer />``
+      <Footer />
     </>
   );
 }
